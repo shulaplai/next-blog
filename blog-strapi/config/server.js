3a6bccad3b989dc61e38c749b7c94b44f1eab703
Your Name <you@example.com>
2021-09-24 17:36:28 +0800

    use the one provided by strapi

--- blog-strapi/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'f3ebc5f0e56ac77d10861eb9db89cd67'),
-    },
-  },
-});
--- blog-strapi/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '673b1f7e-0f3c-4968-a412-cc4b5663f428'
-};
