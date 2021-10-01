07291c2bf0b05edca074e7883b7c100a62780676
Your Name <you@example.com>
2021-10-01 20:01:40 +0800

    Create plugins.js

---
+++ backend/config/plugins.js
@@ -0,0 +1,16 @@
+module.exports = ({ env }) => ({
+  // ...
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: env("hdxqvjx8d"),
+      api_key: env("226391955645232"),
+      api_secret: env("mM68VrokMFI-EzeTwGWh-x9HUnI"),
+    },
+    actionOptions: {
+      upload: {},
+      delete: {},
+    },
+  },
+  // ...
+});
